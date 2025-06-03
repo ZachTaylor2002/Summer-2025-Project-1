79defbb7bddcd4654f03d0fbac4988ce344cbd4a
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2025-06-03 13:49:45 -0500

    feat(appointment): improve time slot formatting for booking UI by using Intl.DateTimeFormat to remove seconds and enhance display clarity

--- Frontend/src/pages/Appointment.jsx
+++ Frontend/src/pages/Appointment.jsx
@@ -4,22 +4,93 @@ import { AppContext } from '../context/AppContext'
 import { assets } from '../assets/assets'
 
 const Appointment = () => {
-  
+
   //First we must get the Doctors'ID
   const {docId} = useParams()
   const {doctors, currencySymbol} = useContext(AppContext)
+  const daysofWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
 
   const [docInfo, setDocInfo] = useState(null)
+
+  //Going to create the time slots for appoinments here
+  const [docSlots, setDocSlots] = useState([])
+  const [slotindex, setSlotIndex] = useState(0)
+  const [slotTime, setSlotTime] = useState('')
 
   const fetchDocInfo = async () => {
     const docInfo = doctors.find(doc => doc._id === docId)
     setDocInfo(docInfo)
-    console.log(docInfo)
+  }
+
+  //This function here will contain the logic to get the avaiable Slots
+  const getAvailableSlots = async () => {
+
+    //First we must clear the previous slots 
+    setDocSlots([])
+
+    //getting current Date
+    let today = new Date()
+    let allSlots = [];
+
+    for(let index = 0; index < 7; index++){
+
+      // getting date with index
+      let currentDate = new Date(today)
+      currentDate.setDate(today.getDate()+index)
+
+      // settings and time of the date with index
+      let endTime = new Date()
+      endTime.setDate(today.getDate()+index)
+      endTime.setHours(21,0,0,0)
+
+      // setting hours 
+      if ( today.getDate() === currentDate.getDate() ){
+        currentDate.setHours(currentDate.getHours() > 10 ? currentDate.getHours() + 1 : 10 )
+        currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30 : 0)
+      } 
+      else{
+        currentDate.setHours(10)
+        currentDate.setMinutes(0)
+      }
+
+      let timeSlots = []
+
+      while( currentDate < endTime ){
+        let formattedTime = new Intl.DateTimeFormat('en-US', {
+          hour: 'numeric',
+          minute: 'numeric',
+          hour12: true
+        }).format(currentDate)
+
+        //add slot to array
+        timeSlots.push({
+          datetime: new Date(currentDate),
+          time: formattedTime
+        })
+
+        // Increment current time by 30 minutes
+        currentDate.setMinutes(currentDate.getMinutes() + 30)
+      }
+      allSlots.push(timeSlots)
+    
+      setDocSlots(allSlots)
+
+    }
+
   }
 
   useEffect(()  => {
     fetchDocInfo()
   }, [doctors, docId])
+
+  useEffect (() => {
+    getAvailableSlots();
+
+  },[docInfo])
+
+  useEffect(() => {
+    console.log(docSlots);
+  },[docSlots])
 
   return docInfo && (
     <div>
@@ -47,6 +118,33 @@ const Appointment = () => {
             Appointment fee: <span className='text-gray-600'>{currencySymbol}{docInfo.fees}</span>
             </p>
         </div>
+      </div>
+
+      {/* ---- Booking Slots -------  */}
+      <div className='sm:ml-72 sm:pl-4 mt-4 font-medium text-gray-700'>
+        <p>Booking Slots</p>
+        <div className='flex gap-3 items-center w-full overflow-x-scroll mt-4'>
+          {
+            docSlots.length && docSlots.map((item,index) => (
+              <div onClick={()=> setSlotIndex(index)} className={`text-center py-6 min-w-16 rounded-full cursor-pointer ${slotindex === index ? 'bg-primary text-white' : 'border border-gray-200'}`} key={index}>
+                <p>{item[0] && daysofWeek[item[0].datetime.getDay()] }</p>
+                <p>{item[0] && item[0].datetime.getDate()}</p>
+              </div>
+            ))
+          }
+        </div>
+
+        <div className='flex items-center gap-3 w-full overflow-x-scroll mt-4 '>
+          {docSlots.length && docSlots[slotindex].map((item,index)=>(
+            <p onClick={()=> setSlotTime(item.time)} className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer ${item.time === slotTime ? 'bg-primary text-white' : 'text-gray-400 border border-gray-300 '}`} key={index}>
+              {item.time.toLowerCase()}
+
+
+            </p>
+          ))}
+
+        </div>
+        <button className='bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6'> Book an appointment </button>
       </div>
         
     </div>
