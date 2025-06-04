2a7e40e4c7219578b33aecc391c29ca8fa597206
Ryan Thieu <150309976+RT-1848@users.noreply.github.com>
2025-06-04 13:32:53 -0500

    Slight change to About

--- Frontend/src/pages/About.jsx
+++ Frontend/src/pages/About.jsx
@@ -10,31 +10,33 @@ const About = () => {
         </p>
       </div>
 
-      <div className='my-10 flex flex-col md:flex-row gap-12'>
-        <img className='w-full md:max-w-[360px]' src={assets.about_image} alt="" />
-        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
-          <p>Welcome to ZRLink, your trusted partner in managing your healthcare needs conveniently and efficiently! At ZRLink, we understand the challenges individuals face when it comes to scheduling doctor appointments and managing their health records.</p>
-          <p>ZRLink is commited to excellence in healthcare technology. We continuously strive to enhance our platform, integrating the latest advancements to improve user experience and deliver superior service. Whether you're booking your first appointment or managing ongoing care, ZRLink is here to supprot you every step of the way!</p>
-          <b className='text-gray-800'>Our Vision</b>
-          <p>At ZRLink, we want to create a seamless healthcare experience for every user. We aim to bridge the gap between patients and healthcare providers, making it easier for you all to access the care you need and when you need it!</p>
+      <div className="flex items-center justify-center pl-20">
+        <div className='my-10 flex flex-col md:flex-row gap-12'>
+          <img className='w-full md:max-w-[360px]' src={assets.about_image} alt="" />
+          <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
+            <p>Welcome to ZRHealthLink, your trusted partner in managing your healthcare needs conveniently and efficiently! At ZRHealthLink, we understand the challenges individuals face when it comes to scheduling doctor appointments and managing their health records.</p>
+            <p>ZRHealthLink is commited to excellence in healthcare technology. We continuously strive to enhance our platform, integrating the latest advancements to improve user experience and deliver superior service. Whether you're booking your first appointment or managing ongoing care, ZRHealthLink is here to supprot you every step of the way!</p>
+            <b className='text-gray-800'>Our Vision</b>
+            <p>At ZRHealthLink, we want to create a seamless healthcare experience for every user. We aim to bridge the gap between patients and healthcare providers, making it easier for you all to access the care you need and when you need it!</p>
+          </div>
         </div>
       </div>
 
       <div className='text-xl my-4'>
-        <p>WHY <span className='text-gray-700 font-semibold'>CHOOSE US</span></p>
+        <p>WHY <span className='text-gray-700 font-semibold'>CHOOSE US?</span></p>
       </div>
 
       <div className='flex flex-col md:flex-row mb-20'>
         <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
-          <b>Efficiency:</b>
+          <b className='text-2xl'>Efficiency:</b>
           <p>Streamlined appointment scheduling that fits into your busy lifestyle</p>
         </div>
         <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
-          <b>Convenience:</b>
+          <b className='text-2xl'>Convenience:</b>
           <p>Access to a network of trusted healthcare professionals in your area.</p>
         </div>
         <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
-          <b>Personalization:</b>
+          <b className='text-2xl'>Personalization:</b>
           <p>Tailored recommendations and reminders to help you stay on top of your health!</p>
         </div>
       </div>
