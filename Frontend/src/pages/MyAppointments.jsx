71f64f75dc4b94bf95730ec698f1f12ae713f634
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2025-06-04 14:46:53 -0500

    Structured and Layed out the User's Appoinment React Component

--- Frontend/src/pages/MyAppointments.jsx
+++ Frontend/src/pages/MyAppointments.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
 
-const MyAppointments = () => {
+const MyAppointments = () => { 
+
+  const { doctors } = useContext(AppContext)
+
   return (
     <div>
-        
+        <p>My appointments</p>
+        {/* We will display the appointments here */}
+        <div>
+          {doctors.slice(0,2).map((item,index)=> (
+            <div key={index}>
+              <div>
+                <img src={item.image} alt="" />
+              </div>
+              <div>
+                <p>{item.name}</p>
+                <p>{item.specialty}</p>
+                <p>Address</p>
+                <p>{item.address.line1}</p>
+                <p>{item.address.line2}</p>
+                <p><span>Date & Time:</span> 25, July, 2025 | 8:30PM</p>
+              </div>
+              <div></div>
+              <div>
+                <button>Pay Online</button>
+                <button>Cancel appointment</button>
+              </div>
+
+            </div>
+          ))}
+        </div>
     </div>
   )
 }
