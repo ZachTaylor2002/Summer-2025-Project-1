07a0f1520019cffc16425eb81c1806f2bb815f77
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2025-06-09 14:23:26 -0500

    Created the Pages and Components for the Admin Panel side

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,14 +4,27 @@ import {ToastContainer, toast} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { useContext } from 'react'
 import { AdminContext } from './context/AdminContext'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import { Route } from 'react-router-dom'
+import Dashboard from './pages/Admin/Dashboard'
 
 const App = () => {
 
   const {aToken} = useContext(AdminContext)
 
   return aToken ? (
-    <div>
+    <div className='bg-[#F8F9FD]'>
       <ToastContainer/>
+      <Navbar/>
+      <div className='flex items-start'>
+        <Sidebar />
+        <Routes>
+          <Route path='/' element={<></>}/>
+          <Route path='/admin-dashboard' element={<Dashboard/>}/>
+          {/* Start Here! :)))  */}
+        </Routes>
+      </div>
 
     </div>
   ) : (
