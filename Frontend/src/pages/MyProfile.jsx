6ed7a444cd249aeebd7b2c73229f714fb57c28f8
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2025-06-13 14:16:57 -0500

    Added Tailwind CSS properties to fix the spacing and format of the edit user profile section

--- Frontend/src/pages/MyProfile.jsx
+++ Frontend/src/pages/MyProfile.jsx
@@ -19,9 +19,9 @@ const MyProfile = () => {
         {
           isEdit
           ? <label htmlFor="image">
-            <div>
-              <img src={image ? URL.createObjectURL(image): userData.image} alt="" />
-              <img src={image ? '': assets.upload_icon} alt="" />
+            <div className='inline-block relative cursor-pointer'>
+              <img className='w-36 rounded opacity-75' src={image ? URL.createObjectURL(image): userData.image} alt="" />
+              <img className='w-10 absolute bottom-12 right-12' src={image ? '': assets.upload_icon} alt="" />
             </div>
             <input onChange={(e) => setImage(e.target.files[0])} type="file" id="image" hidden />
           </label>
