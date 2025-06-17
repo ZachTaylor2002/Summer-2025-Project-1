3c77d9ebdc44fac36153ec87fb52ba6c04633297
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2025-06-17 12:04:51 -0500

    Created the appointmentAdmin API to get the appointments from the database

--- Backend/controllers/adminController.js
+++ Backend/controllers/adminController.js
@@ -3,6 +3,7 @@ import bcrypt, { hash } from "bcrypt"
 import { v2 as cloudinary } from "cloudinary"
 import doctorModel from "../models/doctorModel.js"
 import jwt from 'jsonwebtoken'
+import appointmentModel from "../models/appointmentModels.js"
 
 //API to Add Doctor
 const addDoctor = async (req, res) => {
@@ -97,4 +98,17 @@ const allDoctors = async (req, res) => {
     }
 }
 
-export {addDoctor, loginAdmin, allDoctors}
+//API to get all the appoinment list
+const appointmentsAdmin = async (req, res) => {
+
+    try {
+        const appointments = await appointmentModel.find({})
+        res.json({success:true, appointments})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
+}
+
+export {addDoctor, loginAdmin, allDoctors, appointmentsAdmin}
