762aeebc52fb6017f957c61249a8dfd95bf26728
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2025-06-19 18:54:52 -0500

    feat: implement doctor dashboard API to fetch earnings, appointments, and latest activity

--- Backend/controllers/doctorController.js
+++ Backend/controllers/doctorController.js
@@ -122,4 +122,44 @@ const appointmentCancel = async (req, res) => {
     }
 }
 
-export {changeAvailability, doctorList, loginDoctor, appointmentsDoctor,  appointmentComplete, appointmentCancel}
+//API to get the Dashboard Data for doctor panel
+const doctorDashboard = async (req,res) => {
+
+    try {
+        const {docId} = req.body
+
+        const appointments = await appointmentModel.find({docId})
+
+        let earnings = 0
+
+        //Calculating the earnings of each Doctor
+        appointments.map((item)=>{
+            if (item.isCompleted || item.payment){
+                earnings += item.amount
+            }
+        })
+
+        let patients = []
+
+        appointments.map(()=>{
+            if(!patients.includes(item.userId) ){
+                patients.push(item.userId)
+            }
+        })
+
+        const dashData = {
+            earnings,
+            appointments: appointments.length,
+            patients: patients.length,
+            latestAppointments: appointments.reverse().slice(0,5)
+        }
+
+        res.json({sucess:true, dashData})
+        
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
+}
+
+export {changeAvailability, doctorList, loginDoctor, appointmentsDoctor,  appointmentComplete, appointmentCancel, doctorDashboard}
--- admin/src/pages/Doctor/DoctorDashboard.jsx
+++ admin/src/pages/Doctor/DoctorDashboard.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const DoctorDashboard = () => {
     return (
-        <div>DoctorDashboard</div>
+        <div>
+            
+        </div>
     )
 }
 
